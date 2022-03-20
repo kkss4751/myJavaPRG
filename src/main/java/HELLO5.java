8be11e17aa3c678c689e237c9e54b1eff6f6232b
jeonghun <kkss4751@naver.com>
2022-03-20 15:08:53 +0100

    복습

---
+++ src/main/java/HELLO5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HELLO5 {
+    public static void main(String[] args) {
+        /*
+        int a, b;
+        int result;
+        Scanner s = new Scanner(System.in);
+        System.out.print("첫번째 계산할 값을 입력하세요 ==>");
+        a = s.nextInt();
+        System.out.print("두번쨰 계산할 값을 입력하세요 ==>");
+        b = s.nextInt();
+        result = a + b;
+        result = a + b;
+        */
+        System.out.println("안녕하세요 자바입니다");
+        System.out.println("만나서 반가워요");
+
+        System.out.print("안녕하세요 자바입니다");
+        System.out.print("만나서 반가워요");
+
+    }
+}
